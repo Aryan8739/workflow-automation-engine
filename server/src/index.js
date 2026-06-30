256413bf534d4ae297424340fcc1bf3e01d1e748
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-06-30 20:49:54 +0530

    current cors origin set to Localhost

--- server/src/index.js
+++ server/src/index.js
@@ -14,9 +14,9 @@ import './workers/runWorker.js';
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: 'http://localhost:5173', methods: ['GET', 'POST'] } });
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
 // Routes
