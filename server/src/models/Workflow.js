bee5ddcc3255e1fdf660596490e79380677f45ed
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2026-06-30 20:51:52 +0530

    storing node position on canvas

--- server/src/models/Workflow.js
+++ server/src/models/Workflow.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 const nodeSchema = new mongoose.Schema({
   id: { type: String, required: true },
   type: { type: String, enum: ['http', 'transform', 'delay'], required: true },
-  config: { type: mongoose.Schema.Types.Mixed, default: {} }
+  config: { type: mongoose.Schema.Types.Mixed, default: {} },
+  position: {
+    x: { type: Number, required: true, default: 0 },
+    y: { type: Number, required: true, default: 0 }
+  }
 }, { _id: false });
 
 const edgeSchema = new mongoose.Schema({
